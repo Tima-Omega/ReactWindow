8d222fd8e508c5e7820f6032980b27a2ee560e17
Tima Omega <tima_omega@outlook.com>
2021-11-05 05:46:53 +0300

    add route & header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import './App.css';
+import './styles/main.scss';
+import "./styles/normalize.css"
+import Header from './components/header';
+// import About from './components/about';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Header />
   );
 }
 
